880794db4045762579ffe27c64f2a3f70205f96b
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-05-20 18:40:31 +0200

    remove drop admin users table from migration

--- db/migrate/20180509052144_drop_admin_users.rb
+++
@@ -1,7 +0,0 @@
-class DropAdminUsers < ActiveRecord::Migration[5.2]
-  def change
-    def change
-      drop_table :admin_users
-    end
-  end
-end
--- db/migrate/20180509052543_drop_duplicate_tables.rb
+++
@@ -1,6 +0,0 @@
-class DropDuplicateTables < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :admin_users
-
-  end
-end
