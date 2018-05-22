5faa890409f9267dd6b5d8457f0c53fde00cd370
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-05-22 07:55:13 +0200

    fix testimonials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
    config.action_mailer.delivery_method = :smtp
 
    ActionMailer::Base.smtp_settings = {
-   :user_name => ENV['app97143201@heroku.com'],
-   :password => ENV['ogfgggbo1366'],
+   :user_name => ENV['USERNAME'],
+   :password => ENV['PASSWORD'],
    :domain => 'heroku.com',
    :address => 'smtp.sendgrid.net',
    :port => 587,
