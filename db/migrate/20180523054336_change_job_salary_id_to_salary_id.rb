2daeb7bacf676590b50635fe050bf1fd3401377e
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-05-23 08:01:25 +0200

    dont migrate for job salary id

--- db/migrate/20180523054336_change_job_salary_id_to_salary_id.rb
+++ db/migrate/20180523054336_change_job_salary_id_to_salary_id.rb
@@ -1,7 +1,7 @@
 class ChangeJobSalaryIdToSalaryId < ActiveRecord::Migration[5.2]
   def change
     add_column :jobs, :salary_id, :integer
-    remove_column :jobs, :job_salary_id
+    # remove_column :jobs, :job_salary_id
 
   end
 end
