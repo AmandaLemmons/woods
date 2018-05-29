e2f4cae484ef0ca855b0cb1ffbf5dd9109e7d483
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-05-29 11:11:07 +0100

    add colors to titles, and call modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,9 @@
 //= require jquery
 //= require jquery_ujs
-
 // require popper
 //= require bootstrap-sprockets
-
 //= require bootstrap
-
-
 //= require activestorage
-
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
