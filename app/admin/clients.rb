c1a2e75574204f54660f52786c72283f67f25709
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-06-11 09:06:14 +0200

    add clients to active admin

---
+++ app/admin/clients.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Client do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :company, :address, :phone_number, :email, :comments, :other, :contact_name, :status, :manager_id
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
