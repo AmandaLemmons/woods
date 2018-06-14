e172d2ed7af768f0b00e8505fe0c48990d933d51
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-06-14 07:16:00 +0200

    add colors to active clients

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,31 @@
 module ClientsHelper
+
+
+
+  def color_client_status(status)
+    if status == "active"
+      "btn-outline-primary"
+    elsif status =="inactive"
+      "btn-outline-danger"
+    else
+      "btn-outline-warning"
+    end
+  end
+
+
+  # def color_client_status(status)
+  #   if status == "active"
+  #     haml_tag :class => "btn" do
+  #       haml_tag :class => "btn-success"
+  #     end
+  #   elsif status =="inactive"
+  #     haml_tag :class => "btn" do
+  #       haml_tag :class => "btn-warning"
+  #     end
+  #   else
+  #     haml_tag :class => "btn" do
+  #       haml_tag :class => "btn-warning"
+  #     end
+  #   end
+  # end
 end
