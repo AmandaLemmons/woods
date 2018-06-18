e660e2af6886c3510022d4256ac8945ac7f08b7a
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-06-18 06:49:17 +0200

    change active admin manager page

--- app/admin/managers.rb
+++ app/admin/managers.rb
@@ -2,14 +2,19 @@ ActiveAdmin.register Manager do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :email, :password
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+permit_params :email, :password, :password_confirmation
+index do
+    column :email
+    actions
+  end
+
+  form do |f|
+    f.inputs 'Manager' do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 
 end
